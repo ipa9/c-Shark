43e190681af28bbb0b369356b741014c2f22828c
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2023-11-24 11:03:33 +0400

    Create Context.cs

---
+++ quiz-3/Context.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Lecture> Lectures { get; set; }
+    public string DbPath { get; }
+}
