ced4343ec57bc119614ebdae2355558d2811dd53
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2023-11-24 11:04:11 +0400

    Update Context.cs

--- quiz-3/Context.cs
+++ quiz-3/Context.cs
@@ -15,8 +15,6 @@ public class ApplicationContext : DbContext
         DbPath = System.IO.Path.Join(path, "blogging.db");
     }
 
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
 
