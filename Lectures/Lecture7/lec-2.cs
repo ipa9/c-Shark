5e54b71457a5aaef54527596f89b86ffbec3bf8b
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2023-11-24 09:46:33 +0400

    Create lec-2.cs

---
+++ Lectures/Lecture7/lec-2.cs
@@ -0,0 +1,7 @@
+var numbers = new List<int>() { 1,2,3,7,8,9,10,12,14,15,16};
+var evenNumberSquared = numbers.Where(x => x % 2 == 0)
+    .Select(x => Math.Pow(x, 2)); ;
+
+Console.WriteLine("--------");
+foreach (var number in evenNumberSquared)
+    Console.WriteLine(number);
